f3aa64b1fd72809391fba7285a3918289dd41910
rahulraj0402 <rahul3004rt@gmail.com>
2024-02-07 20:35:01 +0530

    added

---
+++ src/main/java/com/rahul/JobMatchPro/JobMatchProApplication.java
@@ -0,0 +1,15 @@
+package com.rahul.JobMatchPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMatchProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMatchProApplication.class, args);
+		System.out.println(" ");
+		System.out.println();
+	}
+
+}
---
+++ src/test/java/com/rahul/JobMatchPro/JobMatchProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.JobMatchPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMatchProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
