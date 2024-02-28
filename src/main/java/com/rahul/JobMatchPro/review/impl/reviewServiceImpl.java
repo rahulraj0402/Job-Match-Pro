baa0ed95c8fa8b78cb43c92152cf0b1785bfc070
rahulraj0402 <rahul3004rt@gmail.com>
2024-02-28 16:58:16 +0530

    postgresql

--- src/main/java/com/rahul/JobMatchPro/review/impl/reviewServiceImpl.java
+++ src/main/java/com/rahul/JobMatchPro/review/impl/reviewServiceImpl.java
@@ -13,8 +13,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+
 
 
 @AllArgsConstructor
